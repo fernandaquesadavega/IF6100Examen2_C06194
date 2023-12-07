4162f9e4bf0d12359d7f557a104d795ad9e33102
fernandaquesadavega <150489270+fernandaquesadavega@users.noreply.github.com>
2023-12-06 23:40:51 -0600

    Iclusion de pruebas de resta y division

--- src/main/java/cr/go/ucr/Main.java
+++ src/main/java/cr/go/ucr/Main.java
@@ -24,7 +24,44 @@ public class Main {
        System.out.println("Multiplicación: " + multip );
 	   System.out.println("División: " + divis );
    
-     System.out.println("Fin del proceso " );  
+       
+	   
+	   
+	   System.out.println("\nPrueba de los datos resta");
+       context = new OperacionAritmetica( new Resta() );
+       int resta1 = context.procesar(4,20);
+       context = new OperacionAritmetica( new Resta() );
+       int resta2 = context.procesar(-5,10);
+       context = new OperacionAritmetica( new Resta() );
+       int resta3 = context.procesar(10,10);
+       context = new OperacionAritmetica( new Resta() );
+       int resta4 = context.procesar(10,4);
+       System.out.println("Resta1: " + resta1 );
+       System.out.println("Resta2: " + resta2 );
+       System.out.println("Resta3: " + resta3 );
+       System.out.println("Resta4: " + resta4 );
+       
+       
+       System.out.println("\nPrueba de los datos division");
+       context = new OperacionAritmetica( new Division() );
+       int divis1 = context.procesar(-10,-2);
+       context = new OperacionAritmetica( new Division() );
+       int divis2 = context.procesar(8,0);
+       context = new OperacionAritmetica( new Division() );
+       int divis3 = context.procesar(7,1);
+       context = new OperacionAritmetica( new Division() );
+       int divis4 = context.procesar(10,4);
+       context = new OperacionAritmetica( new Division() );
+       int divis5 = context.procesar(1000000,2);
+       context = new OperacionAritmetica( new Division() );
+       int divis6 = context.procesar(0,0);
+       System.out.println("División1: " + divis1 );
+       System.out.println("División2: " + divis2 );
+       System.out.println("División3: " + divis3 );
+       System.out.println("División4: " + divis4 );
+       System.out.println("División5: " + divis5 );
+       System.out.println("División6: " + divis6 );
 	}
 
 }
+
